85339060083f030c98f22f6fbb09568ba41c4e16
S0Ale <78408616+S0Ale@users.noreply.github.com>
2022-06-28 20:57:46 +0200

    Changes

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,23 @@
+const AUDIO_SAMPLES = 64;
+
 let audio1 = new Audio();
 audio1.src = './res/spell.wav';
+const audioCtx = new AudioContext();
 
-$('#button1').on('click', () => {
+let container = $('#container');
+
+let canvas = $('#canvas')[0];
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
+
+let audioSrc, analyser;
+
+container.on('click', () => {
   audio1.play();
+  audioSrc = audioCtx.createMediaElementSource(audio1);
+  analyser = audioCtx.createAnalyser();
+  analyser.connect(audioCtx.destination);
+  analyser.fftSize = AUDIO_SAMPLES;
+  let bufferLength = analyser.frequencyBinCount;
 });
