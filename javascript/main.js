ab932df8406ce4455202db20e509e11085dab980
S0Ale <78408616+S0Ale@users.noreply.github.com>
2022-06-30 03:28:15 +0200

    Comment cleanup

--- javascript/main.js
+++ javascript/main.js
@@ -12,7 +12,6 @@ canvas.height = window.innerHeight;
 let visual = new Visualiser(canvas, AUDIO_SAMPLES);
 
 fileInput.on('change', function(){
-  //const audioCtx = new AudioContext();
   if(visual.isRendering){
     visual.stop();
     visual.currentAudio.pause();
@@ -23,43 +22,4 @@ fileInput.on('change', function(){
   visual.prepareAudio();
   visual.currentAudio.play();
   visual.start();
-
-  //URL.createObjectURL(files[0])
-
-
-  /*
-  const files = this.files;
-  let audio1 = $('#audio1')[0];
-  audio1.src = URL.createObjectURL(files[0]);
-  audio1.volume = .3;
-  audio1.load();
-  audio1.play();
-
-  audioSrc = audioCtx.createMediaElementSource(audio1);
-  analyser = audioCtx.createAnalyser();
-  audioSrc.connect(analyser);
-  analyser.connect(audioCtx.destination);
-  analyser.fftSize = AUDIO_SAMPLES;
-  let bufferLength = analyser.frequencyBinCount;
-  let data = new Uint8Array(bufferLength);
-
-  const barWidth = canvas.width/bufferLength;
-  let barHeight, x;
-  function animate(){
-    x = 0;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    analyser.getByteFrequencyData(data);
-
-    for(let i = 0; i < bufferLength; i++){
-      barHeight = data[i] * 2;
-      ctx.fillStyle = 'white';
-      ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-      x += barWidth;
-    }
-
-    requestAnimationFrame(animate);
-  }
-
-  animate();
-  */
 });
